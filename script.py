230fb6bd54dd7d983ab1dfea15415fe4bd9ca910
pjo901018 <pjo901018@korea.ac.kr>
2017-03-13 22:38:18 +0900

    initial script

---
+++ script.py
@@ -0,0 +1,21 @@
+import librosa
+import os
+
+import librosa.display
+
+
+dirpath = "data"
+filepath = "하1.m4a"
+path = os.path.join(dirpath, filepath)
+y, sr = librosa.core.load(path) # sr = sampling rate
+
+mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
+print(mfcc.shape)
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10,4))
+librosa.display.specshow(mfcc, x_axis='time')
+plt.colorbar()
+plt.title('MFCC')
+plt.tight_layout()
+plt.show()
