fd82a42645ec6f7d353a18765fd5b631294b077a
pjo901018 <pjo901018@korea.ac.kr>
2017-03-15 22:28:59 +0900

    train 스크립트에 ftp download 추가

--- train_script.py
+++ train_script.py
@@ -4,9 +4,10 @@
 from src.model import Model
 from config.config import HYPARMS
 from DataConverter import inputdata
+from FTP_Manager.downloadfromftp import downloadfromftp
 
 
-
+downloadfromftp()
 model = Model()
 data_sets = inputdata.read_sound_datasets(HYPARMS.train_data_dir,
                                           HYPARMS.test_data_dir,
